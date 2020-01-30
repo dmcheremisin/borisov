9d6aee7674614577bcf52d1b2d96cab8b59632ba
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-01-30 19:48:14 +0300

    Quoter implementation

--- SpringRipperPart1/src/main/java/org/example/Main.java
+++ SpringRipperPart1/src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import org.example.quoters.Quoter;
+import org.example.quoters.TerminatorQuoter;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("context.xml");
+        applicationContext.getBean(TerminatorQuoter.class).sayQuote();
     }
 }
---
+++ SpringRipperPart1/src/main/java/org/example/quoters/Quoter.java
@@ -0,0 +1,7 @@
+package org.example.quoters;
+
+public interface Quoter {
+
+    void sayQuote();
+
+}
