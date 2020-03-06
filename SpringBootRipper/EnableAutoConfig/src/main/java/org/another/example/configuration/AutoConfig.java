8df751110b87d099737e58d51fef22ae52cc8d64
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-03-06 14:12:33 +0300

    SpringBoot production condition 2 times

--- SpringBootRipper/EnableAutoConfig/src/main/java/org/another/example/configuration/AutoConfig.java
+++ SpringBootRipper/EnableAutoConfig/src/main/java/org/another/example/configuration/AutoConfig.java
@@ -1,4 +1,4 @@
-package org.another.example;
+package org.another.example.configuration;
 
 import org.another.example.annotation.ConditionOnProduction;
 import org.another.example.listener.RavenListener;
@@ -12,9 +12,17 @@ Without spring.factories file this configuration(AutoConfig.class) will not be a
 public class AutoConfig {
 
     @Bean
-    @ConditionOnProduction
+    @ConditionOnProduction // will ask for the first time about - is it production?
     public RavenListener getRavenListener() {
         return new RavenListener();
+    }
+
+    @Bean
+    @ConditionOnProduction // will ask for the second time about - is it production?, so evaluated 2 times instead of 1
+    public String getStringBean() {
+        String stringBean = "String bean requested";
+        System.out.println(stringBean);
+        return stringBean;
     }
 
 }
